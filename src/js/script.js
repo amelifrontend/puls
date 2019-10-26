6a5c10b92ed223110ac98a33b464c55ed8c32720
amelifrontend <ameli.first@gmail.com>
2019-10-26 16:30:34 +0300

    add scroll

--- src/js/script.js
+++ src/js/script.js
@@ -148,5 +148,38 @@
     //маска ввода номера
     $('input[name=phone]').mask("+38 (999) 999-99-99");
 
+    //настройка отправки писем
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth scroll and pageup
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+
 });
   
